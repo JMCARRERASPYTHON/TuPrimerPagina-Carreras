f928204335664aae1df5d6e7e6e4cf01c4bfbfa2
JMCARRERASPYTHON <jmcarreraspython@gmail.com>
2024-01-18 11:28:30 -0300

    Actualización de Altas y Listas

--- project/core/forms.py
+++ project/core/forms.py
@@ -7,3 +7,13 @@ class ProfesorForm(forms.ModelForm):
     class Meta:
         model = models.Profesor
         fields = "__all__"
+
+class EstudianteForm(forms.ModelForm):
+    class Meta:
+        model = models.Estudiante
+        fields = "__all__"
+        
+class CursoForm(forms.ModelForm):
+    class Meta:
+        model = models.Curso
+        fields = "__all__"
